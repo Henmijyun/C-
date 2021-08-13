1be43b6a66d173d880d9d7aff9bea9ab046a9ff0
henmijyun <1106671528@qq.com>
2021-08-14 02:48:57 +0900

    更新する

--- demo_animal_name/mani.c
+++ demo_animal_name/mani.c
@@ -1,6 +1,14 @@
-#include<stdio.h>
+/*#include<stdio.h>
 int main(void)
 {
 	printf("hello,world\n");
 	return 0;
+}*/
+
+#include<stdio.h>
+int main()
+{
+	printf("こんにちは、世界\n");//日本語でもよろしいじゃない？//
+	return 0;
+
 }
