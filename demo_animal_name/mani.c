aa68c6ffd4d45641543870508a73e69f83ba52b6
henmijyun <1106671528@qq.com>
2021-08-15 23:02:45 +0900

    8/15更新

--- demo_animal_name/mani.c
+++ demo_animal_name/mani.c
@@ -6,9 +6,33 @@ int main(void)
 }*/
 
 #include<stdio.h>
-int main()
+/*int main()
 {
-	printf("こんにちは、世界\n");//日本語でもよろしいじゃない？//
+	printf("こんにちは、世界\n");//日本語でもよろしいじゃない？
 	return 0;
 
+}*/
+int main()
+{
+	char ch = 'A';         //char--字符类型  
+	printf("%c\n",ch);     // %c--打印字符格式的数据   \n--换行的意思
+	/* %f--打印浮点数字（打小数）
+	   %p--以地址的形式打印
+	   %x--打印十六进制数字
+	   %o....*/
+	//short int   //短整型
+
+	int age = 20;           //整型
+	printf("%d\n", age);    // %d--打印整型十进制数据
+
+	//long 长整型
+    long  nom = 11100;
+	printf("%d\n", nom);
+	
+	float f = 5.2;     //单精度浮点数
+	printf("%f\n", f);
+	
+	double d = 3.14;    //双精度浮点数
+	printf("%lf\n", d);//打印双精度浮点数时，用%f可能会丢失精度，所以用%lf更为合适
+	return 0;
 }
