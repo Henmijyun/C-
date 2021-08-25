4f68749e8e17390b5411bb1b6f16d13bff261949
henmijyun <1106671528@qq.com>
2021-08-25 19:38:54 +0900

    8/25 更新

--- demo_animal_name/sum.c
+++ demo_animal_name/sum.c
@@ -168,13 +168,86 @@
 //#define 定义标识符常量
 //#define MAX 100
 //#define 可以定义宏 - 带参数
+//#include<stdio.h>
+//int Max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//
+////宏的定义方式
+//#define MAX(X,Y) (X>Y?X:Y) //宏，变成后面的公式   比自定义函数更简洁
+//int main()
+//{
+//	//int a = MAX;
+//	int a = 10;
+//	int b = 20;
+//	//函数
+//	int max = Max(a, b);
+//	printf("max=%d\n", max);
+//	//宏的方式
+//	max = MAX(a, b);
+//	//max=(a>b?a:b);  因为宏的定义，变成这个公式
+//	printf("max=%d\n", max);
+//	return 0;
+//}
+//// 
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 10;//4个字节
+//	int* p = &a;//取地址  int* - 指针变量
+//	printf("%p\n", &a);
+//	printf("%p\n", p);
+//	*p = 20;// * - 解引用操作符     原本*p这个地址是数值是10，这里的=20是把*p地址的数改成20
+//	printf("%d\n", a);//则int a也变20
+//	//有一种变量是用来存放地址的 - 指针变量
+//	//printf("%p\n", &a);
+//
+//	return 0;
+//}
+
+//指针的大小
+//#include<stdio.h>
+//int main()
+//{
+//	char ch = 'w';
+//	char* pc = &ch;//根据类型不同，指针随之改变
+//	printf("%d\n", sizeof(pc));//4 个字节（32位平台）  如果是8个字节，则64位平台
+//	//*pc = 'a';
+//	//printf("%c\n", ch);
+//	printf("%d\n", sizeof(char*));//4
+//	printf("%d\n", sizeof(short*));//4
+//	printf("%d\n", sizeof(int*));//4
+//	printf("%d\n", sizeof(double*));//4
+//	return 0;
+//}
+
+//创建一个结构体类型
+#include<stdio.h>
+struct Book
+{
+	char name[20];//C语言程序设计
+	short price;//55
+};                                 //;分号不可确少
+#include<string.h>
 int main()
 {
-	//int a = MAX;
-	int a = 10;
-	int b = 20;
-	//函数
-	int max = Max(a, b);
-	printf("max=%d\n", max);
+	//利用结构体类型 - 创建一个改类型的变量
+	struct Book b1 = { "C语言程序设计",55 };
+	strcpy(b1.name, "C++");//strcpy - string copy - 字符串拷贝-库函数-string.h
+	printf("%s\n", b1.name);
+	//printf("书名：%s\n", b1.name);
+	//printf("价格：%d元\n", b1.price);
+	//b1.price = 15;                         //可以直接修改
+	//printf("修改后的价格：%d元\n", b1.price);
+	//struct Book* pb = &b1;//这里定义了一个指针类型struct Book 名叫 pb
+	////利用pb打印出我的书名和价格
+	////printf("%s\n", (*pb).name);//方法1   .结构体变量.成员
+	////printf("%d\n", (*pb).price);//(*pb)的（）是为了系统不要误会*pb.name是一个指针变量
+	//printf("%s\n", pb->name);//方法2   ->结构体指针->成员
+	//printf("%d\n", pb->price);
 	return 0;
 }
