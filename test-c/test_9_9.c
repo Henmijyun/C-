4d37b68fb1dde59df04243290151720224976358
henmijyun <1106671528@qq.com>
2021-09-11 09:23:21 +0900

    9/11 更新

--- test-c/test_9_9.c
+++ test-c/test_9_9.c
@@ -35,3 +35,35 @@
 //	printf("%s\n", arr3);
 //	return 0;
 //}
+
+#include<stdio.h>
+Swap1(int x, int y) //传值调用 只是取外面的值来用,不影响外面的值
+{
+	int tmp = 0;
+	tmp = x;
+	x = y;
+	y = tmp;
+}
+Swap2(int* pa, int* pb)  //传址调用 取外面地址,从而影响外面的值
+{
+	int tmp = 0;
+	tmp = *pa;      //解地址
+	*pa = *pb;
+	*pb = tmp;
+}
+int main()
+{
+	int a = 10;
+	int b = 20;
+	printf("a=%d b=%d\n",a,b); //打印改变前
+
+	Swap1(a, b);
+	//调用Swap1函数 - 传值调用
+	printf("a=%d b=%d\n", a, b);  
+
+	Swap2(&a, &b);
+	//调用Swap1函数 - 传址调用
+
+	printf("a=%d b=%d",a,b);  //打印改变后
+	return 0;
+}
