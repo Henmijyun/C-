ab975f3a63ab7aa8b9626e52fa0d924bd62e43f1
henmijyun <1106671528@qq.com>
2021-09-14 22:17:11 +0900

    9/14 更新

---
+++ test_9_14/test.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+//#include"add.h"
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int sum = 0;
+//	sum = Add(a, b);
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+#include<stdio.h>
+int main()
+{
+	printf("hehe\n");
+	main();
+	return 0;
+}
