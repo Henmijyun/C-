f01ed217158a9eefaaae970b778660dfa8c4a292
henmijyun <1106671528@qq.com>
2021-09-15 20:46:27 +0900

    9/15 更新

--- test_9_14/test.c
+++ test_9_14/test.c
@@ -11,11 +11,94 @@
 //	printf("%d\n", sum);
 //	return 0;
 //}
+//
+//#include<stdio.h>
+//int main()
+//{
+//	printf("hehe\n");
+//	main();
+//	return 0;
+//}
+//#include<stdio.h>
+//void print(int n)      //以输入:123 为例：
+//{
+//	if (n > 9)         //第一次123>9进去,123/10=12,第二次12>9再进去,12/10=1,第三次进去1!>9,跳出递归
+//	{
+//		print(n / 10); //第三次时n=1跳出,1%10=1,打印1,返回上一层12%10=2,打印2,再返回上一层123%10=3,打印3
+//	}
+//	printf("%d ", n % 10);//所以得出的结果为1 2 3
+//}
+//
+//int main()
+//{
+//	unsigned int num = 0;
+//	scanf("%d", &num);
+//	//递归
+//	print(num);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<string.h>
+////int my_strlen(char* str)   //这个自定义函数可以实现求字符串长度,但创建了临时变量,而且不是递归
+////{
+////	int count = 0;
+////	while (*str != '\0') //因为从首元素开始,b不是\0,就记录一次,bit共3次,count从0加到了3,再返回count的值
+////	{
+////		count++;
+////		str++;     //str++每+一次,往后走一个单位 b -> i -> t -> \0
+////	}
+////	return count;
+////}
+//int my_strlen(char* str)  //递归的方法
+//{
+//	if (*str != '\0')
+//		return 1 + my_strlen(str + 1);
+//	else
+//		return 0;
+//}
+////把大事化小
+//int main()
+//{
+//	char arr[] = "bit";
+//	//int len = strlen(arr);  //求字符串长度的 库函数
+//	//printf("%d\n", len);
+//	//模拟实现一个求字符串长度的 自定义函数
+//	int len = my_strlen(arr); //arr是数组,数组传参,传过去的不是整个数组,而是第一个元素的地址
+//	printf("len=%d\n", len);  
+//	return 0;
+//}
 
 #include<stdio.h>
+int Fac1(int n)
+{
+	int i = 0;
+	int ret = 1;
+	for (i = 1; i <= n; i++)
+	{
+		ret *= i;
+	}
+	return ret;
+}
+int Fac2(int n)
+{
+	if (n <= 1)
+		return 1;        //例如5，5>1,进去5*Fac2,进去5-1=4，4>1,再进去4*Fac2，如此类推5-4-3-2-1，
+	else
+		return n * Fac2(n - 1);  //当1=1返回1，跳出递归，不断返回值,使1*2*3*4*5，最好返回计算结果
+}
 int main()
 {
-	printf("hehe\n");
-	main();
+	//求n的阶乘
+	int n = 0;
+	int ret1 = 0;
+	int ret2 = 0;
+	scanf("%d", &n);
+
+	ret1 = Fac1(n);//循环的方式
+	printf("%d\n", ret1);
+
+	ret2 = Fac2(n);//递归的方式
+	printf("%d\n", ret2);
 	return 0;
 }
