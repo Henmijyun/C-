d0b7fc0b7d8d1ec7dc67dfc106cd76779559a27b
henmijyun <1106671528@qq.com>
2021-09-28 21:17:15 +0900

    9/28 更新

--- test_9_14/test.c
+++ test_9_14/test.c
@@ -255,26 +255,33 @@
 //	return 0;
 //}
 
-//青蛙跳台阶问题
-#include<stdio.h>
-int frog(int n)
-{
-    if (n == 1)
-    {
-        return 1;
-    }
-    if (n == 2)
-    {
-        return 2;
-    }
-    return frog(n - 1) + frog(n - 2);
-}
+////青蛙跳台阶问题
+//#include<stdio.h>
+//int frog(int n)
+//{
+//    if (n == 1)
+//    {
+//        return 1;
+//    }
+//    if (n == 2)
+//    {
+//        return 2;
+//    }
+//    return frog(n - 1) + frog(n - 2);
+//}
+//int main()
+//{
+//    int n;
+//    scanf("%d", &n);
+//    int ways = frog(n);
+//    printf("%d\n", ways);
+//    return 0;
+//}
+
+//数组
+
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    int ways = frog(n);
-    printf("%d\n", ways);
-    return 0;
+	int a = ;
+	return 0;
 }
-
