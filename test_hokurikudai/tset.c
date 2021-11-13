d084fbb8fac44d72043c638a74018a12bf3507ac
henmijyun <1106671528@qq.com>
2021-11-13 23:36:53 +0900

    11/13 更新

--- test_hokurikudai/tset.c
+++ test_hokurikudai/tset.c
@@ -36,61 +36,100 @@
 
 //Hokuriku University
 
+//第6回
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	char cs[20] = {'H','o','k','u','r','i','k','u',' ','U','n','i','v','e','r','s','i','t','y'};
+//	char ch[20] = { 0 };
+//	int i, j, v;
+//	char c, d;
+//	int b = 1;
+//	int a = 0;
+//	int len = strlen(cs);  //lenはchの長さ
+//	while (b)
+//	{
+//		for (v = 0; v < len; v++)
+//		{
+//			ch[v] = cs[v];
+//			if (v == 20)
+//			{
+//				ch[v] = '\0';
+//			}
+//		}
+//		printf("挿入前の文字列：");
+//		puts(cs);
+//		printf("どの位置の文字の前に挿入しますか？");
+//		scanf("%d", &a);
+//		if (a < 0 || a > len)
+//		{
+//			printf("位置指定が正しくありません\n---------------------------\n");
+//		}
+//		else if (a >= 0 && a <= len)
+//		{
+//			printf("挿入後の文字列：");
+//			for (i = 0; i < len; i++)
+//			{
+//				if (i == a)
+//				{
+//					c = ch[i];
+//					ch[i] = '*';
+//					printf("%c", ch[i]);
+//					for (j = i+1; j < len + 1 ; j++)
+//					{
+//						d = ch[j];
+//						ch[j] = c;
+//						c = d;
+//						printf("%c", ch[j]);
+//					}
+//					printf("\n\n---------------------------\n");
+//					break;
+//				}
+//				printf("%c", ch[i]);
+//			}
+//			printf("もう一回やりませんか（1.もう一回　0.終わり）：");
+//			scanf("%d", &b);
+//			printf("\n---------------------------\n");
+//		}
+//	}
+//	return 0;
+//}
+
+//第7回
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+float My_Flmax(float arr[],int num)
+{
+	int x = 0;
+	float max = arr[0];
+	for (x = 1; x < num; x++)
+	{
+		if (max < arr[x])
+		{
+			max = arr[x];
+		}
+	}
+	return max;
+}
 int main()
 {
-	char cs[20] = {'H','o','k','u','r','i','k','u',' ','U','n','i','v','e','r','s','i','t','y'};
-	char ch[20] = { 0 };
-	int i, j, v;
-	char c, d;
-	int b = 1;
-	int a = 0;
-	int len = strlen(cs);  //lenはchの長さ
-	while (b)
+	int Num = 0;
+	int i = 0;
+	int a = 1;
+	float Arr[50] = { 0 };
+	while (a)
 	{
-		for (v = 0; v < len; v++)
+		printf("入力する数値の数:");
+		scanf("%d", &Num);
+		for (i = 0; i < Num; i++)
 		{
-			ch[v] = cs[v];
-			if (v == 20)
-			{
-				ch[v] = '\0';
-			}
+			printf("%d つ目の数値を入力:", i + 1);
+			scanf("%f", &Arr[i]);
 		}
-		printf("挿入前の文字列：");
-		puts(cs);
-		printf("どの位置の文字の前に挿入しますか？");
+		printf("最大の入力値は、%.2fです。\n------------\nもう一回？(1\\0)", My_Flmax(Arr, Num));
 		scanf("%d", &a);
-		if (a < 0 || a > len)
-		{
-			printf("位置指定が正しくありません\n---------------------------\n");
-		}
-		else if (a >= 0 && a <= len)
-		{
-			printf("挿入後の文字列：");
-			for (i = 0; i < len; i++)
-			{
-				if (i == a)
-				{
-					c = ch[i];
-					ch[i] = '*';
-					printf("%c", ch[i]);
-					for (j = i+1; j < len + 1 ; j++)
-					{
-						d = ch[j];
-						ch[j] = c;
-						c = d;
-						printf("%c", ch[j]);
-					}
-					printf("\n\n---------------------------\n");
-					break;
-				}
-				printf("%c", ch[i]);
-			}
-			printf("もう一回やりませんか（1.もう一回　0.終わり）：");
-			scanf("%d", &b);
-			printf("\n---------------------------\n");
-		}
+		printf("\n\n");
 	}
 	return 0;
 }
