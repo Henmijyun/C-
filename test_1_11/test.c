869665e582364680be058adfd4178f6f2fb2dd51
henmijyun <1106671528@qq.com>
2022-01-14 12:44:34 +0900

    1/14 更新

--- test_1_11/test.c
+++ test_1_11/test.c
@@ -44,12 +44,68 @@
 //    return 0;
 //}
 
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 2;
+//	int b = -a;
+//	int c = +b;
+//	printf("%d %d %d\n", a, b, c);
+//	return 0;
+//}
+
+//#include<stdio.h>
+////创建结构体
+//struct Sut
+//{
+//    float a[5];
+//    float b[5];
+//    float c[5];
+//    float d[5];
+//    float e[5];
+//}s[5] = { 0 };//创建结构体变量
+//
+//int main()
+//{
+//
+//    int i = 0;
+//    for (i = 0; i < 5; i++)
+//    {
+//        //输入
+//        scanf("%f %f %f %f %f", (s[i]).a, (s[i]).b, (s[i]).c, (s[i]).d, (s[i]).e);
+//        printf("\n");
+//    }
+//    for (i = 0; i < 5; i++)
+//    {
+//        printf("%.1f %.1f %.1f %.1f %.1f ", (s[i]).a, (s[i]).b, (s[i]).c, (s[i]).d, (s[i]).e);
+//        printf("%.1f\n", ((s[i]).a) + ((s[i]).b) + ((s[i]).c) + ((s[i]).d) + ((s[i]).e));
+//    }
+//    return 0;
+//}
+
+
 #include<stdio.h>
 int main()
 {
-	int a = 2;
-	int b = -a;
-	int c = +b;
-	printf("%d %d %d\n", a, b, c);
-	return 0;
+    double Sut[5][5] = { 0 };
+    int i = 0;
+    int j = 0;
+    double add = 0;
+    for (i = 0; i < 5; i++)
+    {
+        scanf("%lf %lf %lf %lf %lf", Sut[i][0], Sut[i][1], Sut[i][2], Sut[i][3], Sut[i][4]);
+   
+    }
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 5; j++)
+        {
+            printf("%.1f ", Sut[i][j]);
+            add += Sut[i][j];
+        }
+        printf("%.1f\n", add);
+        add = 0;
+    }
+
+    return 0;
 }
