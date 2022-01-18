219c1fdb56cc85d2975587f02d62392f9e02fb3d
henmijyun <1106671528@qq.com>
2022-01-18 10:45:22 +0900

    1/18 更新

--- test_1_11/test.c
+++ test_1_11/test.c
@@ -83,29 +83,58 @@
 //    return 0;
 //}
 
+//
+//#include<stdio.h>
+//int main()
+//{
+//    double Sut[5][5] = { 0 };
+//    int i = 0;
+//    int j = 0;
+//    double add = 0;
+//    for (i = 0; i < 5; i++)
+//    {
+//        scanf("%lf %lf %lf %lf %lf", Sut[i][0], Sut[i][1], Sut[i][2], Sut[i][3], Sut[i][4]);
+//   
+//    }
+//    for (i = 0; i < 5; i++)
+//    {
+//        for (j = 0; j < 5; j++)
+//        {
+//            printf("%.1f ", Sut[i][j]);
+//            add += Sut[i][j];
+//        }
+//        printf("%.1f\n", add);
+//        add = 0;
+//    }
+//
+//    return 0;
+//}
+
+
+//计算1 / 1 - 1 / 2 + 1 / 3 - 1 / 4 + 1 / 5 …… + 1 / 99 - 1 / 100 的值，打印出结果
+
 
 #include<stdio.h>
 int main()
 {
-    double Sut[5][5] = { 0 };
-    int i = 0;
-    int j = 0;
-    double add = 0;
-    for (i = 0; i < 5; i++)
-    {
-        scanf("%lf %lf %lf %lf %lf", Sut[i][0], Sut[i][1], Sut[i][2], Sut[i][3], Sut[i][4]);
-   
-    }
-    for (i = 0; i < 5; i++)
-    {
-        for (j = 0; j < 5; j++)
-        {
-            printf("%.1f ", Sut[i][j]);
-            add += Sut[i][j];
-        }
-        printf("%.1f\n", add);
-        add = 0;
-    }
+	int i = 1;
+	int add = 0;
 
-    return 0;
+	while (i < 100)   //1 3 5 7 9  i最大99
+	{
+		add += ((100 / i) - (100 / (i + 1)));
+		i += 2; 
+	}
+	printf("%d/100\n", add);
+	return 0;
 }
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	int add = 110;
+//
+//	printf("%d/100\n", add);
+//	return 0;
+//}
