12ea40ea845d1760ae02cf1eb367293953d55c45
henmijyun <1106671528@qq.com>
2022-01-20 01:46:30 +0900

    1/20 更新

--- test_1_11/test.c
+++ test_1_11/test.c
@@ -114,20 +114,20 @@
 //计算1 / 1 - 1 / 2 + 1 / 3 - 1 / 4 + 1 / 5 …… + 1 / 99 - 1 / 100 的值，打印出结果
 
 
-#include<stdio.h>
-int main()
-{
-	int i = 1;
-	int add = 0;
-
-	while (i < 100)   //1 3 5 7 9  i最大99
-	{
-		add += ((100 / i) - (100 / (i + 1)));
-		i += 2; 
-	}
-	printf("%d/100\n", add);
-	return 0;
-}
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	float add = 0;
+//
+//	while (i < 100)   //1 3 5 7 9  i最大99
+//	{
+//		add += ((1.0 / i) - (1.0 / (i + 1)));
+//		i += 2; 
+//	}
+//	printf("%f\n", add);
+//	return 0;
+//}
 
 //#include<stdio.h>
 //int main()
@@ -136,5 +136,188 @@ int main()
 //	int add = 110;
 //
 //	printf("%d/100\n", add);
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//void menu()
+//{
+//	printf("***************\n");
+//	printf("*****1.play****\n");
+//	printf("*****0.exit****\n");
+//	printf("***************\n");
+//}
+//
+//void game()
+//{
+//	//1.生成一个随机数
+//	int guess = 0;
+//	int ret = rand()%100+1;
+//	//2.猜数字
+//	printf("猜数字：》");
+//	while (1)
+//	{
+//		scanf("%d", &guess);
+//		if (guess < ret)
+//			printf("猜小了\n");
+//		else if (guess > ret)
+//			printf("猜大了\n");
+//		else
+//		{
+//			printf("猜对了\n");
+//			break;
+//		}
+//	}
+//}
+//int main()
+//{
+//	int input = 0;
+//	srand((unsigned int)time(NULL));
+//	do
+//	{
+//		menu();
+//		printf("请选择》：");
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			game();
+//			break;
+//		case 0:
+//			printf("退出游戏\n");
+//			break;
+//		default:
+//			printf("输入错误，重新输入");
+//			break;
+//		}
+//	} while (input);
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 0, b = 0;
+//	for (a = 1, b = 1; a <= 100; a++)
+//	{
+//		if (b >= 20) break;
+//		if (b % 3 == 1)
+//		{
+//			b = b + 3;
+//			continue;
+//		}
+//		b = b - 5;
+//	}
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//
+//#include <stdio.h>
+//int main()
+//{
+//	int x = 0;
+//	int y = 0;
+//	int z = 0;
+//	int tmp = 0;
+//	scanf("%d %d %d", &x, &y, &z);
+//	if (x < y)
+//	{
+//		tmp = y;
+//		y = x;
+//		x = tmp;
+//	}
+//	if (x < z)
+//	{
+//		tmp = z;
+//		z = x;
+//		x = tmp;
+//	}
+//	if (y < z)
+//	{
+//		tmp = z;
+//		z = y;
+//		y = tmp;
+//	}
+//	printf("%d %d %d\n", x, y, z);
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int m = 0;
+//	//输入
+//	scanf("%d %d", &a, &b);
+//	while (m = a % b)
+//	{
+//		a = b;
+//		b = m;
+//	}
+//	printf("%d\n", b);
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("%d", printf("%d\n", printf("%d  \n", printf("%d\n", 669))));
+//	return 0;
+//}
+
+//实现一个函数，判断一个数是不是素数。
+//利用上面实现的函数打印100到200之间的素数。
+//
+//#include <stdio.h>
+//int PriNum(int x)
+//{
+//	int i = 0;
+//	for (i = 2; i < x; i++)
+//	{
+//		if (x % i == 0)
+//			return 0;
+//	}
+//	return 1;
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		if (1 == PriNum(i))
+//			printf("素数\n");
+//		else
+//			printf("不是素数\n");
+//	}
+//	return 0;
+//}
+
+
+//实现函数判断year是不是润年。
+//#include <stdio.h>
+//int BisTile(int x)
+//{
+//	if ((x % 400 == 0) || (x % 4 == 0 && x % 100 != 0))
+//		return 1;
+//	else
+//		return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1000; i <= 2000; i++)
+//	{
+//		if (1 == BisTile(i))
+//			printf("%d ",i);
+//	}
+//
 //	return 0;
 //}
