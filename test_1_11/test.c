90a4733a8e97dcf3f2ed7a0cbd7fefc878022f13
henmijyun <1106671528@qq.com>
2022-01-30 00:49:16 +0900

    1/30 更新

--- test_1_11/test.c
+++ test_1_11/test.c
@@ -688,65 +688,65 @@
 
 
 
-//
-//#define _CRT_SECURE_NO_WARNINGS 1
-//#include "game.h"
-//
-//void menu()
-//{
-//	printf("****************\n");
-//	printf("**** 1.play ****\n");
-//	printf("**** 0.exit ****\n");
-//	printf("****************\n");
-//}
-//
-//void game()
-//{
-//	//扫雷游戏的实现
-//	char mine[ROWS][COLS] = { 0 }; //'0'  布置雷
-//	char show[ROWS][COLS] = { 0 }; //'*'  排查雷
-//
-//	//初始化棋盘
-//	init_board(mine, ROWS, COLS, '0');
-//	init_board(show, ROWS, COLS, '*');
-//
-//	//打印棋盘
-//	//show_board(mine, ROW, COL); //看见雷
-//	//show_board(show, ROW, COL); //隐藏雷
-//
-//	//布置雷
-//	set_mine(mine, ROW, COL);
-//	show_board(show, ROW, COL);
-//
-//	//排查雷
-//	fine_mine(mine, show, ROW, COL);
-//}
-//int main()
-//{
-//	int input = 0;
-//	srand((unsigned int)time(NULL));
-//
-//	do
-//	{
-//		menu();
-//		printf("请输入:>\n");
-//		scanf("%d", &input);
-//		switch (input)
-//		{
-//		case 1:
-//			game();
-//			break;
-//		case 0:
-//			printf("退出游戏\n");
-//			break;
-//		default:
-//			printf("输入有误,请重新输入!\n");
-//			break;
-//		}
-//	} while (input);
-//	return 0;
-//}
-//
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "game.h"
+
+void menu()
+{
+	printf("****************\n");
+	printf("**** 1.play ****\n");
+	printf("**** 0.exit ****\n");
+	printf("****************\n");
+}
+
+void game()
+{
+	//扫雷游戏的实现
+	char mine[ROWS][COLS] = { 0 }; //'0'  布置雷
+	char show[ROWS][COLS] = { 0 }; //'*'  排查雷
+
+	//初始化棋盘
+	init_board(mine, ROWS, COLS, '0');
+	init_board(show, ROWS, COLS, '*');
+
+	//打印棋盘
+	//show_board(mine, ROW, COL); //看见雷
+	//show_board(show, ROW, COL); //隐藏雷
+
+	//布置雷
+	set_mine(mine, ROW, COL);
+	show_board(show, ROW, COL);
+
+	//排查雷
+	fine_mine(mine, show, ROW, COL);
+}
+int main()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+
+	do
+	{
+		menu();
+		printf("请输入:>\n");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("输入有误,请重新输入!\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
+
 
 
 
