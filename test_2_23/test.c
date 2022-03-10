b44c20b324cd899e5f1861c682aa6c1b8813289c
henmijyun <1106671528@qq.com>
2022-03-10 12:34:23 +0900

    3/10 更新

--- test_2_23/test.c
+++ test_2_23/test.c
@@ -424,45 +424,30 @@
 
 
 //杨辉三角
-#include <stdio.h>
-
-int main()
-{
-	int arr[10][10] = { 0 };
-	int i = 0;
-	for (i = 0; i < 10; i++)
-	{
-
-	}
-
-
-	return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[10][10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j <10; j++)
+//		{
+//			if (j == 0)  //赋值最左的竖行
+//				arr[i][j] = 1;
+//			if ((i == j) && (j != 0)) //赋值最右的斜行
+//				arr[i][j] = 1;
+//			if (i >= 2 && j >= 1)  //赋值中间部分
+//				arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+//			if (arr[i][j] != 0)  //打印不为0的部分
+//				printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//
 
 
 
