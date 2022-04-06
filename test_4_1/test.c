514fe14df5b4b298995f14cb39e2db1abc21b137
henmijyun <1106671528@qq.com>
2022-04-06 14:38:42 +0900

    4/6 更新

--- test_4_1/test.c
+++ test_4_1/test.c
@@ -1,32 +1,45 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-#include <stdlib.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//	int* ptr = (int*)malloc(10* sizeof(int));
+//	if (NULL != ptr)
+//	{
+//		//使用空间
+//		//..
+//	}
+//	//拓展扩容
+//	int* ptr2 = (int*)realloc(ptr, 100 * sizeof(int));
+//	if (NULL != ptr2)
+//	{
+//		//防止ptr被置为NULL，导致空间丢失
+//		ptr = ptr2;
+//		ptr2 = NULL;
+//	}
+//	else
+//		perror("realloc");
+//	//使用空间
+//	//..
+//	//归还空间
+//	free(ptr);
+// 	ptr = NULL;
+//	return 0;
+//}
+//
+//
+//
 
-int main()
+
+typedef struct st_type 
 {
-	int* ptr = (int*)malloc(10* sizeof(int));
-	if (NULL != ptr)
-	{
-		//使用空间
-		//..
-	}
-	//拓展扩容
-	int* ptr2 = (int*)realloc(ptr, 100 * sizeof(int));
-	if (NULL != ptr2)
-	{
-		//防止ptr被置为NULL，导致空间丢失
-		ptr = ptr2;
-		ptr2 = NULL;
-	}
-	else
-		perror("realloc");
-	//使用空间
-	//..
-	//归还空间
-	free(ptr);
- 	ptr = NULL;
-	return 0;
-}
+	int i;
+	int a[0];//柔性数组成员 
+}type_a;
 
-
-
+typedef struct st_type
+{
+	int i;
+	int a[];//柔性数组成员 
+}type_a;
